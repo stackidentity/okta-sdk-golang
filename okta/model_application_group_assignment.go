73d54369131bde6ec91cd5b8a6af045eb6d4897b
Harshad Joshi <harshad.joshi@stackidentity.com>
2023-10-27 23:48:43 +0530

    BUG-APPUSER-PROFILE-FIX
    # BUG-APPUSER-PROFILE-FIX
    
    - Change the type for _AppUser.Profile from `map[string]map[string]interface{}` to `UserProfile` in model_app_user.go.
    - Change the type for _ApplicationGroupAssignment.Profile from`map[string]map[string]interface{}` to `GroupProfile` in model_application_group_assignment.go

--- okta/model_application_group_assignment.go
+++ okta/model_application_group_assignment.go
@@ -34,7 +34,7 @@ type ApplicationGroupAssignment struct {
 	Id                   *string                           `json:"id,omitempty"`
 	LastUpdated          *time.Time                        `json:"lastUpdated,omitempty"`
 	Priority             *int32                            `json:"priority,omitempty"`
-	Profile              map[string]map[string]interface{} `json:"profile,omitempty"`
+	Profile              *GroupProfile                     `json:"profile,omitempty"`
 	Embedded             map[string]map[string]interface{} `json:"_embedded,omitempty"`
 	Links                *LinksSelf                        `json:"_links,omitempty"`
 	AdditionalProperties map[string]interface{}
@@ -156,9 +156,9 @@ func (o *ApplicationGroupAssignment) SetPriority(v int32) {
 }
 
 // GetProfile returns the Profile field value if set, zero value otherwise.
-func (o *ApplicationGroupAssignment) GetProfile() map[string]map[string]interface{} {
+func (o *ApplicationGroupAssignment) GetProfile() *GroupProfile {
 	if o == nil || o.Profile == nil {
-		var ret map[string]map[string]interface{}
+		var ret *GroupProfile
 		return ret
 	}
 	return o.Profile
@@ -166,7 +166,7 @@ func (o *ApplicationGroupAssignment) GetProfile() map[string]map[string]interfac
 
 // GetProfileOk returns a tuple with the Profile field value if set, nil otherwise
 // and a boolean to check if the value has been set.
-func (o *ApplicationGroupAssignment) GetProfileOk() (map[string]map[string]interface{}, bool) {
+func (o *ApplicationGroupAssignment) GetProfileOk() (*GroupProfile, bool) {
 	if o == nil || o.Profile == nil {
 		return nil, false
 	}
@@ -183,7 +183,7 @@ func (o *ApplicationGroupAssignment) HasProfile() bool {
 }
 
 // SetProfile gets a reference to the given map[string]map[string]interface{} and assigns it to the Profile field.
-func (o *ApplicationGroupAssignment) SetProfile(v map[string]map[string]interface{}) {
+func (o *ApplicationGroupAssignment) SetProfile(v *GroupProfile) {
 	o.Profile = v
 }
 
